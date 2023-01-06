cb7dae417c15c0d26380e710c30d3f76a03dd1b9
Yasser Fadl <yasser.fadl@gmail.com>
2023-01-06 10:05:37 +0000

    chore: updating next

--- components/page-title.tsx
+++ components/page-title.tsx
@@ -5,7 +5,7 @@ import * as React from 'react'
 export const PageTitle: React.FunctionComponent<{ title: string, subTitle: string }> = ({ title, subTitle }) => {
     return <>
     <Head>
-          <title>Vramework.io: {title}</title>
+          <title>{`Vramework.io: ${title}`}</title>
           <meta name="description" content={subTitle} />
       </Head>
       <h1 className="mt-6 w-full text-5xl leading-tight md:text-5xl font-semibold text-center md:leading-snug">
